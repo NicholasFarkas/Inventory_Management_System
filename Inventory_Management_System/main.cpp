4069119ec7e08f6a633e688f7a7462122baf5748
Nicholas Farkas <walter7236@gmail.com>
2020-05-12 16:44:43 -0400

    Initial Commit

---
+++ Inventory_Management_System/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Inventory_Management_System
+//
+//  Created by Nicholas Farkas on 5/12/20.
+//  Copyright © 2020 Nicholas Farkas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
